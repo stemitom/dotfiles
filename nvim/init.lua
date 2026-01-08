2862cf9ebe11898ef04ecbb686629128e1dec3a1
Samuel Adebowale <stemitom@gmail.com>
2026-01-08 01:00:00 +0100

    Auto-sync dotfiles: 2026-01-08 01:00:00

--- nvim/init.lua
+++ nvim/init.lua
@@ -327,7 +327,7 @@ require('lazy').setup {
       vim.g.gruvbox_material_cursor = 'auto'
       vim.g.gruvbox_material_colors_override = { bg0 = '#121212' }
       vim.g.gruvbox_material_better_performance = 1
-      -- vim.cmd.colorscheme 'gruvbox-material'
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 
@@ -345,7 +345,7 @@ require('lazy').setup {
           variables = false,
         },
       }
-      vim.cmd.colorscheme 'vesper'
+      -- vim.cmd.colorscheme 'vesper'
     end,
   },
 

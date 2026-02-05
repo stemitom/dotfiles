01202a82a3efe015f12ff845b47a864913b95253
Samuel Adebowale <stemitom@gmail.com>
2026-02-06 00:00:00 +0100

    Auto-sync dotfiles: 2026-02-06 00:00:00

--- wezterm/shell.lua
+++ wezterm/shell.lua
@@ -1,7 +1,8 @@
 local M = {}
 
 function M.update_config(config)
-  config.default_prog = {"/usr/bin/fish", "-l", "-c", "zellij attach -c"}
+	-- config.default_prog = {"/usr/bin/fish", "-l", "-c", "zellij attach -c"}
+	config.default_prog = { "/usr/bin/fish", "-l", "-c", "tmux new -A -s wezterm" }
 end
 
 return M
